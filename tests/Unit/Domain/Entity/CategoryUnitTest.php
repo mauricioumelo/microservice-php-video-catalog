c5044927a3f08213c0ca52344cbcfcb1dd70165a
mauricioumelo <mauricioumelo@gmail.com>
2023-01-27 11:11:08 -0300

    test: test attributes Category

--- tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -7,5 +7,16 @@ use PHPUnit\Framework\TestCase;
 
 class CategoryUnitTest extends TestCase
 {
-   
+    public function test_attributes()
+    {
+        $category = new Category(
+            name: 'New Category',
+            description: 'this is description of New Category',
+            isActive: true
+        );
+
+        $this->assertEquals('New Category', $category->name);
+        $this->assertEquals('this is description of New Category', $category->description);
+        $this->assertEquals(true, $category->isActive);
+    }
 }
