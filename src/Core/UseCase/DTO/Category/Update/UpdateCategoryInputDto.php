94d5ccd5e2d7c1b2e241260a15b548b543a6ec6e
mauricioumelo <mauricioumelo@gmail.com>
2023-11-25 21:20:24 +0000

    feat: adiciona parâmetros no construtor

--- src/Core/UseCase/DTO/Category/Update/UpdateCategoryInputDto.php
+++ src/Core/UseCase/DTO/Category/Update/UpdateCategoryInputDto.php
@@ -5,8 +5,10 @@ namespace Core\UseCase\DTO\Category\Update;
 class UpdateCategoryInputDto
 {
     public function __construct(
-       public string $id,
-       public array $data
+        public string $id,
+        public string $name,
+        public string|null $description = null,
+        public bool $isActive = true,
     ) {
     }
 }
