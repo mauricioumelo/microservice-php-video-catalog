1ff8b6a35016a4765986e52ec32b5c90baf3b153
mauricioumelo <mauricioumelo@gmail.com>
2023-11-26 19:33:49 -0300

    feat: adiciona metodo para validar valores nulos

--- src/Core/UseCase/DTO/Category/Update/UpdateCategoryInputDto.php
+++ src/Core/UseCase/DTO/Category/Update/UpdateCategoryInputDto.php
@@ -7,8 +7,26 @@ class UpdateCategoryInputDto
     public function __construct(
         public string $id,
         public string $name,
-        public string|null $description = null,
-        public bool $isActive = true,
+        public ?string $description = null,
+        public ?bool $isActive = null,
     ) {
+    }
+
+    public function getdata(): array
+    {
+        $data = [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+
+        if (!is_null($this->description)) {
+            $data['description'] = $this->description;
+        }
+        
+        if (!is_null($this->isActive)) {
+            $data['isActive'] = $this->isActive;
+        }
+
+        return $data;
     }
 }
